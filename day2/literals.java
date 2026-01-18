a357d395d7f061837382fbebf8c0808d2b407542
AbhishekGupta0164 <abhishekgupta0164@gmail.com>
2026-01-18 16:04:26 +0530

    Fix folders: convert from submodules to normal folders

---
+++ day2/literals.java
@@ -0,0 +1,24 @@
+public class literals {
+    public static void main(String[] args) {
+        byte age = 34;
+        char ch = 'A';
+        float f1 = 4.5f;
+        double d1 = 5.6d;
+        short s1 = 56;
+        long ageDino = 732961793693L;
+        boolean a = true;
+        double d2 = 3.456789;
+        String str = "This is a string";
+        System.out.println("My age is: " + age);
+        System.out.println(str);
+        System.out.println("The value of ch is: " + ch);
+        System.out.println("The value of f1 is: " + f1);    
+        System.out.println("The value of d1 is: " + d1);
+        System.out.println("The value of s1 is: " + s1);
+        System.out.println("The value of ageDino is: " + ageDino);
+        System.out.println("The value of a is: " + a);
+        System.out.println("The value of d2 is: " + d2);
+
+    }
+
+}
