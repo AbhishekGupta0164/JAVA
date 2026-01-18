37eeeb338c8070984014573ffd6a533fdc44fe73
AbhishekGupta0164 <abhishekgupta0164@gmail.com>
2026-01-18 21:35:15 +0530

    practics questions

---
+++ day2/practice questions/hello03.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class hello03 {
+    public static void main(String[] args) {
+        // Question 1: Write a Java program to find the sum of three numbers.
+        int a = 67;
+        int b = 90;
+        int c = 83;
+        int sum = a + b + c;
+        System.out.println("The sum of the three numbers is: " + sum);
+
+        // QUESTION 2: Write a Java program to calculate the CGPA of a student given the
+        // marks of three subjects (out of 100).
+        // float subject1 = 45;
+        // float subject2 = 95;
+        // float subject3 = 48;
+        // float cgpa =(subject1 + subject2 + subject3)/30;
+        // System.out.println(cgpa);
+
+        // QUESTION 3: Write a Java program to print "Hello <name>, have a good day"
+        // where <name> is the name entered by the user.
+        // System.out.println("What is your name?");
+        // Scanner sc = new Scanner(System.in);
+        // String name = sc.next();
+        // System.out.println("hello " + name + " have a good day");
+
+        // QUESTION 4: Write a Java program to conver km to miles
+        // System.out.println("Enter the value in kilometers: ");
+        // Scanner sc = new Scanner(System.in);
+        // float km = sc.nextFloat();
+        // float miles = km * 0.621371f;
+        // System.out.println("The value in miles is: " + miles);
+
+        // QUESTION 5: Write a Java program to detect whether a number entered by the
+        // user is an integer or not.
+        // System.out.println("Enter a number: ");
+        // Scanner sc = new Scanner(System.in);
+        // boolean b1 = sc.hasNextInt();
+        // System.out.println(b1);
+    }
+}
