3f30e1115051c49a2e790118dd3ae57a953cc3ae
AbhishekGupta0164 <abhishekgupta0164@gmail.com>
2026-01-18 22:56:34 +0530

    operators

---
+++ day2/operators.java
@@ -0,0 +1,23 @@
+public class operators {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 6;
+        System.out.println("The value of a + b is: " + (a + b));
+        System.out.println("The value of a - b is: " + (a - b));
+        System.out.println("The value of a * b is: " + (a * b));
+        System.out.println("The value of a / b is: " + (a / b));
+        System.out.println("The value of a % b is: " + (a % b)); // modulo operator
+        System.out.println("The value of a++ is: " + (a++));
+        System.out.println("The value of a-- is: " + (a--));
+        System.out.println("The value of ++a is: " + (++a));
+        System.out.println("The value of --a is: " + (--a));
+        System.out.println(a > b); // comparison operators
+        System.out.println(a < b);
+        System.out.println(a > b && b > a);
+        System.out.println(a > b || b > a); // logical operator OR
+        System.out.println(a & b); // bitwise AND
+        System.out.println(a | b); // bitwise OR
+
+    }
+
+}
