40beb084e954d6d2f63fb1cdf717ae6fde06ecdd
AbhishekGupta0164 <abhishekgupta0164@gmail.com>
2026-01-19 22:37:17 +0530

    Expressions and Increment & Decrement operator

---
+++ day3/expression.java
@@ -0,0 +1,28 @@
+
+public class expression {
+    public static void main(String[] args) {
+
+        // byte x = 5;
+        // int y = 6;
+        // short z = 8;
+        // int a = y + z;
+        // float b = 6.65f + x;
+        // System.out.println(a);
+        // System.out.println(b);
+
+        // Increment & Decrement Operators
+        int i = 56;
+        System.out.println("i++ = " + i++);
+        System.out.println("i = " + i);
+        System.out.println("++i = " + ++i);
+        System.out.println("i = " + i);
+
+        // int y = 7;
+        // System.out.println(++y * 8);
+
+        // char ch = 'A';
+        // System.out.println(++ch);
+
+    }
+
+}
