33123c23e5ae7aa6cb738a62598c1b4af90ec042
AbhishekGupta0164 <abhishekgupta0164@gmail.com>
2026-01-20 15:02:46 +0530

    expressions

--- day3/practice questions/compariso_operator.java
+++ day3/practice questions/compariso_operator.java
@@ -1,14 +1,22 @@
 
-import java.util.Scanner;
-
 public class compariso_operator {
     public static void main(String[] args) {
-        // Use comparison operator to find outwhether a gven number is greator than user
-        // entered number or not
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        // System.out.println("Enter number 2nd: ");
-        System.out.println(a > 6);
+        /*
+         * // Use comparison operator to find outwhether a gven number is greator than
+         * user
+         * // entered number or not
+         * 
+         * Scanner sc = new Scanner(System.in);
+         * int a = sc.nextInt();
+         * System.out.println(a > 6);
+         * 
+         */
+
+        // write expression in java
+        // System.out.println("(v^2 - u^2) / 2a5");
+
+        float m = (((7 * 49) / 7) + (35 / 7));
+        System.out.println(m);
 
     }
 }
