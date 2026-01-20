f13c1ac2f9fff4e3f8df8394086552361bf061d4
AbhishekGupta0164 <abhishekgupta0164@gmail.com>
2026-01-20 16:05:35 +0530

    strings in java

---
+++ day4/string.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class string {
+    public static void main(String[] args) {
+
+        // String name = new String("Abhi");
+        // System.out.print("the name is: ");
+        // System.out.print(name);
+
+        // String name = "abhi";
+        // System.out.println("My name is : ");
+        // System.out.println(name);
+
+        int a = 7;
+        float b = 3903.3f;
+        System.out.printf("the value of a is %d and value of b is %f ", a, b);
+        System.out.format("the value of a is %d and value of b is %f ", a, b);
+
+    }
+
+}
