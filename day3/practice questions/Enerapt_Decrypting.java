7f086f54322fed493fb22a7ad2b4b2980ddc4d2b
AbhishekGupta0164 <abhishekgupta0164@gmail.com>
2026-01-20 14:48:37 +0530

    Enerapt_Decrypting

--- day3/practice questions/Enerapt_Decrypting.java
+++ day3/practice questions/Enerapt_Decrypting.java
@@ -3,7 +3,7 @@ public class Enerapt_Decrypting {
     public static void main(String[] args) {
         // question 2- program to enerapt a grade by adding 8 to it , Decrypt it to show
         // correct grade
-        char grade = '8';
+        char grade = '9';
         grade = (char) (grade + 8);
         System.out.println(grade);
 
