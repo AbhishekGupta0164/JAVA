c987d30e81bfd1e1f108ec2417c6ae974ad01dc9
AbhishekGupta0164 <abhishekgupta0164@gmail.com>
2026-01-21 22:45:09 +0530

    String Methods in java

--- day4/StringMethods.java
+++ day4/StringMethods.java
@@ -1,5 +1,42 @@
-package day4;
 
 public class StringMethods {
+    public static void main(String[] args) {
+        String name = "Abhi";
+        // System.out.println(name);
+        int value = name.length();
+        System.out.println(value);
 
+        // String lstring = name.toLowerCase();
+        // System.out.println(lstring);
+
+        // String ustring = name.toUpperCase();
+        // System.out.println(ustring);
+
+        // String nonTrimmedString = " Abhi ";
+        // System.out.println(nonTrimmedString);
+        // String trimmedString = nonTrimmedString.trim();
+        // System.out.println(nonTrimmedString.trim());
+
+        // System.out.println(name.substring(1));
+        // System.out.println(name.substring(1, 3));
+
+        // System.out.println(name.replace('i', 'r'));
+
+        // System.out.println(name.startsWith("ab"));
+        // System.out.println(name.endsWith("i"));
+
+        // System.out.println(name.charAt(3));
+        // System.out.println(name.indexOf("A"));
+
+        // String modifiedString = "Abhishek";
+        // System.out.println(modifiedString.indexOf("shek", 4));
+        // System.out.println(modifiedString.indexOf("shek"));
+        // System.out.println(modifiedString.lastIndexOf("k"));
+
+        // System.out.println(name.equals("abhi"));
+        // System.out.println(name.equalsIgnoreCase("abhi")); // no matter of upper /
+        // lower case
+
+        // System.out.println("I am escape sequence \\ double quote");
+    }
 }
