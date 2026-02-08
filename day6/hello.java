784dadbaa4b07b384260edd4de12bcd590ba0128
AbhishekGupta0164 <abhishekgupta0164@gmail.com>
2026-02-08 22:34:20 +0530

    Code for Rock Paper Scissors Game

---
+++ day6/hello.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+// code for rock paper scissors game
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Rock Paper Scissors Game!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter choice of 1st player: ");
+        String player1 = sc.next();
+        System.out.println("Enter choice of 2nd player: ");
+        String player2 = sc.next();
+
+        if (player1.equals(player2)) {
+
+            System.out.println("It's a tie!");
+
+        } else if (player1.equals("rock") && player2.equals("scissors")
+                || player1.equals("paper") && player2.equals("rock")
+                || player1.equals("scissors") && player2.equals("paper")) {
+            System.out.println("Player 1 wins!");
+
+        } else {
+            System.out.println("Player 2 wins!");
+        }
+    }
+}
