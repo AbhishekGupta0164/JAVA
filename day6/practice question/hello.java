6d6d90314dfcacd4be72030aa6dbc5d232e440be
AbhishekGupta0164 <abhishekgupta0164@gmail.com>
2026-02-15 23:47:07 +0530

    Practice Questions on loop

---
+++ day6/practice question/hello.java
@@ -0,0 +1,54 @@
+
+public class hello {
+    public static void main(String[] args) {
+
+        // practice question 1
+        // int n = 4;
+        // for (int i = n; i > 0; i--) {
+        // for (int j = 0; j < i; j++) {
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
+
+        // practice question 2
+        // int sum = 0;
+        // int n = 4;
+        // for (int i = 0; i < n; i++) {
+        // sum = sum + (2 * i);
+
+        // }
+        // System.out.println("sum of first " + n + " even numbers is: " + sum);// 0 2 4
+        // 6 =12
+
+        // practice question 3
+        // int multiply;
+        // int n = 5;
+        // for (int i = 0; i <= 10; i++) {
+        // multiply = n * i;
+        // System.out.println("Multiplication of table of " + n + " * " + i + " is : " +
+        // multiply);
+        // }
+
+        // practice question 4 table in reverse order
+        // int multiply;
+        // int n = 10;
+        // for (int i = 10; i >= 0; i--) { // 10 to 0
+        // multiply = n * i;
+        // System.out.println("Multiplication of table of " + n + " * " + i + " is : " +
+        // multiply);
+        // }
+
+        // practice question 5 factorial of number
+        // fact n = n * (n-1) * (n-2) * (n-3) ..........
+        // int fact = 1;
+        // int n = 4;
+        // for (int i = 1; i <= n; i++) {
+        // fact = fact * i;
+        // }
+        // System.out.println("Factorial of " + n + " is: " + fact); // 4* 3 * 2 * 1 =
+        // 24
+
+    }
+
+}
