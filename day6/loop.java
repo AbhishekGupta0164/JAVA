18facb8e4fc33166620dbdcb0c664f18cf1ac0cd
AbhishekGupta0164 <abhishekgupta0164@gmail.com>
2026-02-15 21:51:15 +0530

    Rock,Paper,Scissor Game using if else loop.

--- day6/loop.java
+++ day6/loop.java
@@ -1,8 +1,21 @@
-package day6;
 
 public class loop {
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+        int i = 1;
+        while (i <= 3) {
+            System.out.println("using loops");
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Loop Done");
+
+        // while (true) {
+        // System.out.println("i am a infinite while loop");
+        // }
+
     }
 
 }
---
+++ day6/loop2.java
@@ -0,0 +1,22 @@
+public class loop2 {
+    public static void main(String[] args) {
+
+        // WHILE LOOP
+        // int a = 10;
+        // while (a < 6) {
+        // System.out.println(a);
+        // a++;
+        // }
+        // System.out.println("Loop Done");
+
+        // ḌO WHILE LOOP
+        // int b = 10;
+        // do {
+        // System.out.println(b);
+        // b++;
+
+        // } while (b < 6);
+
+    }
+
+}
