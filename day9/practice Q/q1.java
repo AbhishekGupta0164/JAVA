a87356882e1529a53cc1a836a84a26c0226686ef
AbhishekGupta0164 <abhishekgupta0164@gmail.com>
2026-03-01 19:43:43 +0530

    To  create a class cylinder , use getters & setters to set radius & height and to find surface area and volume

--- day9/practice Q/q1.java
+++ day9/practice Q/q1.java
@@ -1,4 +1,5 @@
-// question 1 - create a class cylinder and use getters & setters to set radius and height
+// question 1 - create a class cylinder and use getters & setters to set radius and height 
+//  & to find surface area and volume
 
 class cylinder {
     private int radius;
@@ -20,14 +21,29 @@ class cylinder {
         return height;
     }
 
+    // to find surface area
+    public double surface_area() {
+        return 2 * Math.PI * radius * height + 2 * 3.14 * radius * radius;
+    }
+
+    public double volume() {
+        return Math.PI * radius * radius * height;
+    }
+
 }
 
 public class q1 {
     public static void main(String[] args) {
+
         cylinder shape = new cylinder();
-        shape.setRadius(25);
-        System.out.println(shape.getRadius());
-        shape.setHeight(15);
-        System.out.println(shape.getHeight());
+        shape.setRadius(9);
+        System.out.println("The radius of cylinder is: " + shape.getRadius());
+        shape.setHeight(12);
+        System.out.println("The height of cylinder is: " + shape.getHeight());
+
+        // to find surface area and volume of cylinder
+        System.out.println("The surface area of cylinder is: " + shape.surface_area()); // 1187.52
+        System.out.println("The volume of cylinder is: " + shape.volume()); // 3053.62
+
     }
 }
