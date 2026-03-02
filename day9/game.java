e4ad8a242710c80d5aee184f61588a8bd23b1bc4
AbhishekGupta0164 <abhishekgupta0164@gmail.com>
2026-03-02 22:59:08 +0530

    Rock, Paper & Scissor Game

--- day9/game.java
+++ day9/game.java
@@ -1,35 +1,55 @@
 import java.util.Random;
 import java.util.Scanner;
 
+// Rock, Paper & Scissor Game 
 public class game {
     public static void main(String[] args) {
         // 0 for rock
         // 1 for paper
         // 2 for seissor
-        Scanner.sc = new Scanner(System.in);
-        System.out.print("Enter 0 for rock, 1 for paper and 2 for scissor");
-        int userinput = sc.nextint();
+        Scanner sc = new Scanner(System.in);
+        String choice;
 
-        Random random = new Random();
-        int computerinput = random.nextInt(3);
+        while (true) { // used for repeat game
 
-        // comparision for output
-        if (userinput == computerinput) {
-            System.out.println("Draw");
-        } else if (userinput == 0 && computerinput == 2 || userinput == 1 && computerinput == 0
-                || userinput == 2 && computerinput == 1) {
-            System.out.println("You Win!!");
-        } else {
-            System.out.println("Computer Win");
+            System.out.println("Enter your choice \n0 for rock, \n1 for paper, \n2 for scissor: ");
 
+            int userinput = sc.nextInt();
+
+            sc.nextLine();
+
+            Random random = new Random();
+            int computerinput = random.nextInt(3);
+
+            // comparision for result
+            if (userinput == computerinput) {
+                System.out.println("Draw");
+            } else if (userinput == 0 && computerinput == 2 || userinput == 1 && computerinput == 0
+                    || userinput == 2 && computerinput == 1) {
+                System.out.println("You Win!!");
+            } else {
+                System.out.println("Computer Win");
+            }
+            if (userinput < 0 || userinput > 2) {
+                System.out.println("Invalid choice! Please enter 0, 1 or 2 only.");
+                continue;
+            }
+            // computer choice
+            if (computerinput == 0) {
+                System.out.println("Computer Choice: ROCK");
+            } else if (computerinput == 1) {
+                System.out.println("Computer Choice: PAPER");
+            } else {
+                System.out.println("Computer Choice: SCISSOR");
+            }
+            System.out.println("Press R to replay or X to exit");
+            choice = sc.nextLine();
+
+            if (choice.equalsIgnoreCase("X")) {
+                System.out.println("Game Ended:)");
+                break;
+            }
         }
-        // outputs
-        if (computerinput == 0) {
-            System.out.println("Computer Choice: ROCK");
-        } else if (computerinput == 1) {
-            System.out.println("Computer Choice: PAPER");
-        } else if (computerinput == 0) {
-            System.out.println("Computer Choice: SCISSOR");
-        }
+        sc.close();
     }
 }
