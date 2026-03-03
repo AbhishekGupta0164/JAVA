fe34d65ea34ef9b8766cf92f4ff7414221663534
AbhishekGupta0164 <abhishekgupta0164@gmail.com>
2026-03-03 21:59:56 +0530

    example

--- day10/practice q/employee.java
+++ day10/practice q/employee.java
@@ -2,6 +2,6 @@
 public class employee {
     public static void main(String[] args) {
 
-        System.out.println("Employee details practice question ");
+        System.out.println("Employee details example practice question ");
     }
 }
