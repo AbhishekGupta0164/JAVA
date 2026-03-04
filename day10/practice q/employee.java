284d052e4279842eb9809251269077c3fa9e8e50
AbhishekGupta0164 <abhishekgupta0164@gmail.com>
2026-03-04 15:54:21 +0530

    Question 1

--- day10/practice q/employee.java
+++
@@ -1,7 +0,0 @@
-
-public class employee {
-    public static void main(String[] args) {
-
-        System.out.println("Employee details example practice question ");
-    }
-}
---
+++ day10/practice q/q1.java
@@ -0,0 +1,36 @@
+// question 1
+/* 1.create class employee 
+2. salary property(int)
+3. getSalary (method returning int)
+4. name property(string)
+5. getname(method returning string)
+6. getname(method changing name)
+ */
+class employee {
+
+    int Salary;
+    String Name;
+
+    public int getSalary() {
+        return Salary;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String n) {
+        Name = n;
+    }
+}
+
+public class q1 {
+    public static void main(String[] args) {
+        employee abhi = new employee();
+        abhi.setName("Abhishek");
+        abhi.Salary = 500;
+        System.out.println(abhi.getSalary());
+        System.out.println(abhi.getName());
+        System.out.println("Employee details example practice question ");
+    }
+}
