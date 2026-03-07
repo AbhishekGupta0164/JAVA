f719a91dfa910681f9e748596eedc725a564d5e1
AbhishekGupta0164 <abhishekgupta0164@gmail.com>
2026-03-07 22:14:30 +0530

    d

---
+++ day12/hello.java
@@ -0,0 +1,31 @@
+
+class Parent1 {
+    public Parent1() {
+        System.out.println(" I am a base class constructor ");
+    }
+
+    public void sayHello() {
+        System.out.println("hello");
+    }
+
+    abstract public void greet();
+}
+
+class Child1 extends Parent1 {
+    @Override
+    public void greet() {
+        System.out.println("good morning");
+    }
+}
+
+abstract class Child2 extends Parent1 {
+    public void th(){
+        sout
+    }
+}
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
