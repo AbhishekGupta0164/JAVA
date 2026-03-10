3c73a95624e56fdadabfc124d31c653910b7382d
AbhishekGupta0164 <abhishekgupta0164@gmail.com>
2026-03-10 19:04:19 +0530

    Abstract class

--- day12/hello.java
+++ day12/hello.java
@@ -1,6 +1,6 @@
-
-class Parent1 {
-    public Parent1() {
+// abstract class example
+abstract class Parent {
+    public Parent() {
         System.out.println(" I am a base class constructor ");
     }
 
@@ -9,23 +9,37 @@ class Parent1 {
     }
 
     abstract public void greet();
+
+    abstract public void greet1();
 }
 
-class Child1 extends Parent1 {
+class Child extends Parent {
     @Override
     public void greet() {
         System.out.println("good morning, this is an overridden file ");
     }
+
+    @Override
+    public void greet1() {
+        System.out.println("good afternoon");
+    }
 }
 
-abstract class Child2 extends Parent1 {
+abstract class Child2 extends Parent {
     public void th() {
+        System.out.println("I am good");
 
     }
 }
 
 public class hello {
     public static void main(String[] args) {
+        Child.sc = new Child();
+
+        // create error as it is a abstract class so no object is created
+        // Child2= new Child2();
+        // Parent = new Parent();
+
         System.out.println("hello");
     }
 }
