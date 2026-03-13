a23638b1c2a22d8e3c93db76268913419494976b
AbhishekGupta0164 <abhishekgupta0164@gmail.com>
2026-03-13 11:18:25 +0530

    Question 1

--- day12/Practice Question/Abstract Class_Q/q1.java
+++ day12/Practice Question/Abstract Class_Q/q1.java
@@ -1,4 +1,30 @@
+// Question 1
+// Create a abstract class Pen and extend it with methods write , refill, changeNib
+abstract class Pen {
+    abstract void write();
+
+    abstract void refill();
+}
+
+class FountainPen extends Pen {
+    void write() {
+        System.out.println("Write");
+    }
+
+    void refill() {
+        System.out.println("Refill");
+    }
+
+    void changeNib() {
+        System.out.println("Changing Nib");
+    }
+}
 
 public class q1 {
-
+    public static void main(String[] args) {
+        FountainPen pen = new FountainPen();
+        pen.changeNib();
+        pen.write();
+        pen.refill();
+    }
 }
