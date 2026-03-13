0f0c811c571f8cc3dc83c979476804be92c23c73
AbhishekGupta0164 <abhishekgupta0164@gmail.com>
2026-03-13 21:16:46 +0530

    Threads

--- day13/Creating a Thread/Th.java
+++ day13/Creating a Thread/Th.java
@@ -1,9 +1,10 @@
-
+//Threads are used to perform multiple tasks simultaneously.
+//In Java, we can create a thread by extending the Thread class or by implementing the Runnable
 class MyThread1 extends Thread {
     @Override
     public void run() {
-        int i = 0;
-        while (i < 332423) {
+        int i = 90;
+        while (i < 100) { // true condition, so this thread will run
             System.out.println("My Thread is Running");
             i++;
         }
@@ -13,16 +14,18 @@ class MyThread1 extends Thread {
 class MyThread2 extends Thread {
     @Override
     public void run() {
-        while (true) {
+        int j = 90;
+        while (j < 20) { // false condition, so this thread will not run
             System.out.println("My Thread2 is chatting");
+            j++;
         }
     }
 }
 
 public class Th {
     public static void main(String[] args) {
-        MyThread1 t1 = new Thread1();
-        MyThread2 t2 = new Thread2();
+        MyThread1 t1 = new MyThread1();
+        MyThread2 t2 = new MyThread2();
         t1.start();
         t2.start();
 
