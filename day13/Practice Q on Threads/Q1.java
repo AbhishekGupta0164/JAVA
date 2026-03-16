5696f4a0433f75f1e0188217e8695e4f3a94cf96
AbhishekGupta0164 <abhishekgupta0164@gmail.com>
2026-03-16 18:33:55 +0530

    To use thread delay, current thread, getState(), Exception, demonstrate getPriority and setPriority methods

---
+++ day13/Practice Q on Threads/Q1.java
@@ -0,0 +1,51 @@
+
+// Q1 - To use Thread delay, current thread, getState(), Exception, demonstrate getPriority and setPriority methods 
+
+class MyThread1 extends Thread {
+    public void run() {
+        while (true) {
+            try {
+                Thread.sleep(200); // Thread delay
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+            System.out.println("Good Morning");
+        }
+    }
+}
+
+class MyThread2 extends Thread {
+    public void run() {
+        while (true) {
+            try {
+                Thread.sleep(200); // Thread delay
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+            System.out.println("Hello, Welcome");
+        }
+    }
+}
+
+public class Q1 {
+    public static void main(String[] args) {
+
+        MyThread1 M1 = new MyThread1();
+        MyThread2 M2 = new MyThread2();
+
+        // Setting priority
+        M1.setPriority(5); // Normal priority
+        M2.setPriority(10); // Maximum priority
+
+        // Getting priority
+        // If priority is not set then by default it is 5 (same for both threads)
+        System.out.println("Priority of Thread 1: " + M1.getPriority());
+        System.out.println("Priority of Thread 2: " + M2.getPriority());
+        System.out.println("State of Thread 2: " + M2.getState());
+        System.out.println("State of Current Thread: " + Thread.currentThread().getState());
+
+        // Starting threads
+        M1.start();
+        M2.start();
+    }
+}
