2d762a0d6b4ad021e00bf796a6f0b0edd68fefef
AbhishekGupta0164 <abhishekgupta0164@gmail.com>
2026-03-23 14:30:57 +0530

    Array Deque

--- day16Adv/Array_Deque/hello.java
+++ day16Adv/Array_Deque/hello.java
@@ -6,6 +6,7 @@ public class hello {
         ArrayDeque<Integer> ad1 = new ArrayDeque<>();
         ad1.add(4);
         ad1.add(5);
+        ad1.add(3);
         ad1.addFirst(42);
         System.out.println(ad1.getFirst());
         System.out.println(ad1.getLast());
