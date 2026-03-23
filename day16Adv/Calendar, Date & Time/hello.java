6500c2a1e0009b94fbb9dd0b9dd19ae488dbd5df
AbhishekGupta0164 <abhishekgupta0164@gmail.com>
2026-03-23 15:45:25 +0530

    Calendar, Date & Time

---
+++ day16Adv/Calendar, Date & Time/hello.java
@@ -0,0 +1,34 @@
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+
+public class hello {
+    public static void main(String[] args) {
+
+        // DATE AND TIME
+        // System.out.println(System.currentTimeMillis() / 1000 / 3600 / 24 / 365);
+
+        /*
+         * It prints the number of years passed from 1900
+         * from 1970 millisecond holds in java
+         * it tells the current year from 1900
+         */
+
+        // CALENDAR
+
+        Calendar c = Calendar.getInstance();
+        System.out.println(c.getCalendarType()); // print type of calendar
+        System.out.println(c.getTimeZone()); // print time zone
+        System.out.println(c.getTimeZone().getID()); // print place of zonal time -> Asia
+        System.out.println(c.getTime());
+        System.out.println("Date is: " + c.get(Calendar.DATE));
+        System.out.println("Seconds : " + c.get(Calendar.SECOND));
+        System.out.println("Hours: " + c.get(Calendar.HOUR));
+        System.out.println("Printing time: " + c.get(Calendar.HOUR_OF_DAY) + ":" + c.get(Calendar.MINUTE) + ":"
+                + c.get(Calendar.SECOND));
+
+        GregorianCalendar gc = new GregorianCalendar();
+        System.out.println(gc.isLeapYear(2026));
+    }
+}
--- day16Adv/Date & Time/hello.java
+++
@@ -1,9 +0,0 @@
-
-public class hello {
-    public static void main(String[] args) {
-        System.out.println(System.currentTimeMillis() / 1000 / 3600 / 24 / 365);
-        // It prints the number of years passed from 1900
-        // from 1970 millisecond holds in java
-        // it tells the current year from 1900
-    }
-}
