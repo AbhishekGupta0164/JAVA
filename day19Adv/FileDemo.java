a26163728d8fe2d740db5021bd62cbc0cef4118b
AbhishekGupta0164 <abhishekgupta0164@gmail.com>
2026-03-31 22:28:30 +0530

    File Handling

---
+++ day19Adv/FileDemo.java
@@ -0,0 +1,47 @@
+
+public class FileDemo {
+    public static void main(String[] args) {
+        // Code to create a new file
+
+        // File myFile = new File("file5.txt");
+        // try {
+        // myFile.createNewFile();
+        // } catch (IOException e) {
+        // System.out.println("Unable to create this file ");
+        // e.printStackTrace();
+        // }
+
+        // code to write in a file
+        // try {
+        // FileWriter filewriter = new FileWriter("file5.txt");
+        // filewriter.write("this is my first file from this java course\nOkay now
+        // byy!!");
+        // filewriter.close();
+        // } catch (IOException e) {
+        // e.printStackTrace();
+        // }
+
+        // code to read a file
+        // File myFile = new File("file5.txt");
+        // try {
+        // Scanner sc = new Scanner(myFile);
+        // while (sc.hasNextLine()) {
+        // String line = sc.nextLine();
+        // System.out.println(line);
+        // }
+        // sc.close();
+        // } catch (FileNotFoundException e) {
+        // System.out.println("File not found");
+        // e.printStackTrace();
+        // }
+
+        // Code to delete a file
+        // File myFile = new File("file5.txt");
+        // if (myFile.delete()) {
+        // System.out.println("I have deleted: " + myFile.getName());
+        // } else {
+        // System.out.println("Some problem occurred while deleting the file");
+        // }
+
+    }
+}
